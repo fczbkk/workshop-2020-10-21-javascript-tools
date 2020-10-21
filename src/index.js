260b5ed73897b9ad5748583c746401cf327d2f15
Riki Fridrich <riki@fczbkk.com>
2020-10-21 16:50:48 +0200

    feat: add documentation

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import { doSomething } from './utilities.js'
 
 console.log('hello world')
 doSomething()
+
+/**
+ * Join two parameters together.
+ * @param {number} first
+ * @param {string} second
+ * @returns {string}
+ */
+function someFunction (first, second) {
+  return first + second
+}
